b98c7157b87b75dd60ba8c1404bebfadf56e830f
Tommy <tommy.hager@yale.edu>
2024-09-28 09:08:47 -0400

    Day5 Start

---
+++ Day5/Day5Part1.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class Day5Part1{
+  public static void main(String[] args){
+    // Read in the input
+    ReadFile rf = new ReadFile();
+    ArrayList<String> inputArray = rf.readFile("./testInput.txt");
+  }
+}
